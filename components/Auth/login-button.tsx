056dae856c8e1582b8ab50ffe44ec5ffb4e29e24
Yoe <pintabot.member@gmail.com>
2024-06-07 23:00:35 +0900

    Refactor login form to reduce spacing, add modal functionality, and update dependencies

--- components/Auth/login-button.tsx
+++ components/Auth/login-button.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { Button } from "../ui/button";  // Adjust the import according to your installation
 import { useRouter } from "next/navigation";
+import { Dialog,
+    DialogContent,
+    DialogTrigger
 
+} from "../ui/dialog";
+import { LoginForm} from "@/components/Auth/login-form";
 interface LoginButtonProps {
     children: React.ReactNode;
     mode?: "modal" | "redirect";
@@ -17,6 +22,20 @@ export const LoginButton = ({
     const onClick = () => {
         router.push("/auth/login");
     }
+   if (mode === "modal") {
+    return (
+      <Dialog>
+        <DialogTrigger>
+          <Button onClick={onClick} variant="outline">
+            {children}
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    );
+   }
    
     return (
         <Button onClick={onClick} variant="outline">
--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { ModeToggle } from "@/components/mode-toggle";
 import { LoginButton } from '../Auth/login-button';
+import { Button } from '../ui/button';
 
 const NavBar = () => {
   return (
@@ -9,13 +10,20 @@ const NavBar = () => {
         <span className="font-semibold text-lg">PejuangKorea</span>
       </div>
       <div className="flex items-center flex-1 justify-center space-x-4">
-        <Link href="#"><span className="hover:text-gray-400">Themes</span></Link>
-        <Link href="#"><span className="hover:text-gray-400">Primitives</span></Link>
-        <Link href="#"><span className="hover:text-gray-400">Icons</span></Link>
-       
+        <Link href="#">
+          <span className="hover:text-gray-400">Themes</span>
+        </Link>
+        <Link href="#">
+          <span className="hover:text-gray-400">Primitives</span>
+        </Link>
+        <Link href="#">
+          <span className="hover:text-gray-400">Icons</span>
+        </Link>
       </div>
-      <ModeToggle/>
-      <LoginButton>Login</LoginButton>
+      <ModeToggle />
+      <LoginButton mode="modal">
+        login
+      </LoginButton>
     </nav>
   );
 }
